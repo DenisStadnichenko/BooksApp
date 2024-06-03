5e90031e9d39331e2c55fc2543c8254be3a24247
Denis Stadnichenko <dvstadnichenko@gmail.com>
2024-06-03 10:56:26 +0300

    remove datastore

--- app/src/main/java/com/books/app/di/DataModule.kt
+++ app/src/main/java/com/books/app/di/DataModule.kt
@@ -2,7 +2,6 @@ package com.books.app.di
 
 import com.books.app.data.api.BookServiceImp
 import com.books.app.data.api.BooksService
-import com.books.app.data.datasource.Preferences
 import com.books.app.data.repository.BooksRepository
 import com.books.app.data.repository.BooksRepositoryImp
 import org.koin.dsl.module
@@ -11,10 +10,6 @@ val dataModule = module {
 
     single<BooksService> {
         BookServiceImp()
-    }
-
-    single<Preferences> {
-        Preferences(get())
     }
 
     single<BooksRepository> {
